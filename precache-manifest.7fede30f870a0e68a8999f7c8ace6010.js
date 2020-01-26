30dec85475f20ac4203a10f795ee8b044e30b67d
NickDunn92 <nickdunn92@icloud.com>
2020-01-26 14:29:45 +0000

    Updates

---
+++ precache-manifest.7fede30f870a0e68a8999f7c8ace6010.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac71a12369b4fc1ff6a790d064ce00f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "057eaacd3d08a1399282",
+    "url": "/my-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d469a54f03806b049cdb",
+    "url": "/my-app/static/css/main.e2a52fe5.chunk.css"
+  },
+  {
+    "revision": "057eaacd3d08a1399282",
+    "url": "/my-app/static/js/2.018c6c94.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/my-app/static/js/2.018c6c94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d469a54f03806b049cdb",
+    "url": "/my-app/static/js/main.1d666a49.chunk.js"
+  },
+  {
+    "revision": "e1a0f1c8466ba6bebb03",
+    "url": "/my-app/static/js/runtime-main.47b76b19.js"
+  },
+  {
+    "revision": "feac02aa6debc7a8f5c6dcff9a5cec35",
+    "url": "/my-app/static/media/UEFASupercup-Bold.feac02aa.woff"
+  }
+]);
